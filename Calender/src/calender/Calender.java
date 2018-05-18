1c9022e46cbc0bc6aeca7d5bc73570598e2966ac
Rakibul islam <33988342+Rakib1604060@users.noreply.github.com>
2018-05-19 01:17:57 +0600

    add files via upload

---
+++ Calender/src/calender/Calender.java
@@ -0,0 +1,28 @@
+
+package calender;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Calender extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root=FXMLLoader.load(getClass().getResource("mainfxml.fxml"));
+        Scene scene =new Scene(root);
+        primaryStage.setTitle("Calender By Rakibul Islam");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }}
